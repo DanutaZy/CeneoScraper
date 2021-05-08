5ad62590feaef4ad60776f8870188ab26198c389
DanutaZy <s219663@student.uek.krakow.pl>
2021-05-08 12:12:50 +0200

    2021-05-08 DZ

--- scraper.py
+++ scraper.py
@@ -1,3 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+
 respons= requests.get('https://www.ceneo.pl/52404834#tab=reviews')
-print (respons.status_code)
+
+page_dom = BeautifulSoup(respons.text,"html.parser")
+
+opinions= page_dom.select("div.js_product-review")
+opinion= opinions.pop(0)
+opinion_id= opinion["data-entry-id"]
+author= opinion.select_one("span.user-post__author-name").text.strip()
+print(author)
+
+#print(page_dom.prettify())
